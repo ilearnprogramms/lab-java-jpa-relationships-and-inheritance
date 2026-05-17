c5363606d75cd2eaf2c3f0d0c1814785bf2a2a2f
Eren <erendogan13@gmail.com>
2026-05-18 01:07:16 +0200

    Final

--- src/main/java/com/jpa/relation/inheritance/Week8/Task1/Model/Name.java
+++ src/main/java/com/jpa/relation/inheritance/Week8/Task1/Model/Name.java
@@ -1,10 +1,15 @@
 package com.jpa.relation.inheritance.Week8.Task1.Model;
 
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 
 @Embeddable
 public class Name {
+
+    @Enumerated(EnumType.STRING)
     private Salutation salutation;
+
     private String firstName;
     private String middleName;
     private String lastName;
